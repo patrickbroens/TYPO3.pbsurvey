912a53e4010da991bbb45825a3ba276cb0d346dd
Patrick Broens <patrick@patrickbroens.nl>
2016-02-17 22:35:10 +0100

    [TASK] Add Item model with type 99
    The item model with the type 99 is missing. This patch adds the item model

---
+++ Classes/Domain/Model/Item/ItemType99.php
@@ -0,0 +1,25 @@
+<?php
+namespace PatrickBroens\Pbsurvey\Domain\Model\Item;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use PatrickBroens\Pbsurvey\Domain\Model\Item\Abstracts\AbstractItem;
+
+/**
+ * Item type 99: Call User Defined Hook
+ */
+class ItemType24 extends AbstractItem
+{
+
+}
