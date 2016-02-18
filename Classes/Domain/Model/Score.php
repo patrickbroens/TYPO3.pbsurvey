9caa5675dddaa84498f0f0c7b20062789f741f2c
Patrick Broens <patrick@patrickbroens.nl>
2016-02-18 20:25:10 +0100

    [TASK] Add Score model
    Page scoring definitions did not have a model yet
    
    This patch adds the model

---
+++ Classes/Domain/Model/Score.php
@@ -0,0 +1,76 @@
+<?php
+namespace PatrickBroens\Pbsurvey\Domain\Model;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * Score
+ */
+class Score extends AbstractModel
+{
+    /**
+     * The page to redirect to
+     *
+     * @var int
+     */
+    protected $page;
+
+    /**
+     * The scored points needed
+     *
+     * @var int
+     */
+    protected $score;
+
+    /**
+     * Get the page to redirect to
+     *
+     * @return int
+     */
+    public function getPage()
+    {
+        return $this->page;
+    }
+
+    /**
+     * Set the page to redirect to
+     *
+     * @param int $page The page uid
+     */
+    public function setPage($page)
+    {
+        $this->page = (int)$page;
+    }
+
+    /**
+     * Get the scored points needed
+     *
+     * @return int
+     */
+    public function getScore()
+    {
+        return $this->score;
+    }
+
+    /**
+     * Set the scored points needed
+     *
+     * @param int $score The scored points needed
+     */
+    public function setScore($score)
+    {
+        $this->score = $score;
+    }
+
+}
