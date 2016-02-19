b8e381ecfb698c70d65208d71981c532102bede7
Patrick Broens <patrick@patrickbroens.nl>
2016-02-19 09:17:13 +0100

    [TASK] Add Score repository
    The Score model is present, but does not have a repository yet
    
    This patch adds the repository

---
+++ Classes/Domain/Repository/ScoreRepository.php
@@ -0,0 +1,78 @@
+<?php
+namespace PatrickBroens\Pbsurvey\Domain\Repository;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use PatrickBroens\Pbsurvey\Domain\Model\Score;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Score repository
+ */
+class ScoreRepository extends AbstractRepository
+{
+    /**
+     * @param int $itemUid The uid of the survey item
+     * @param array $loadObjects The nested models which should be loaded
+     * @return Score[]
+     */
+    public function findByContentElement($contentElementUid, $loadObjects = [])
+    {
+        $scores = [];
+
+        $databaseResource = $this->getDatabaseConnection()->exec_SELECTquery(
+            '
+                uid,
+                page,
+                score
+            ',
+            'tx_pbsurvey_score',
+            '
+                parentid = ' . (int)$contentElementUid . '
+                AND hidden = 0
+                AND deleted = 0
+            ',
+            '',
+            'score ASC'
+        );
+
+        if ($this->getDatabaseConnection()->sql_error()) {
+            $this->getDatabaseConnection()->sql_free_result($databaseResource);
+            return $scores;
+        }
+
+        while ($record = $this->getDatabaseConnection()->sql_fetch_assoc($databaseResource)) {
+            $scores[] = $this->setScoreFromRecord($record, $loadObjects);
+        }
+
+        $this->getDatabaseConnection()->sql_free_result($databaseResource);
+
+        return $scores;
+    }
+
+    /**
+     * Set a score from a database record
+     *
+     * @param array $record The database record
+     * @param array $loadObjects The nested models which should be loaded
+     * @return Score The score
+     */
+    protected function setScoreFromRecord($record, $loadObjects)
+    {
+        $option = GeneralUtility::makeInstance(Score::class);
+        $option->populate($record);
+
+        return $option;
+    }
+}
